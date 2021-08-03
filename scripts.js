e36c4f05e32ab55cd03f64e947f09510f478f406
Leandro Sales <leandrossales.96@gmail.com>
2021-08-03 18:55:05 -0300

    Add not-found search item

--- scripts.js
+++ scripts.js
@@ -1,55 +1,81 @@
 const sectionCards = document.querySelector(".post-section");
-const card = document.querySelector("div.card");
+const searchBar = document.querySelector(".search-bar");
+
+searchBar.addEventListener("keyup", (e) => {
+  const searchString = e.target.value;
+
+  const filteredItems = blogSections.filter((item) => {
+    return (
+      item.title.includes(searchString) ||
+      item.description.includes(searchString)
+    );
+  });
+  displayItems(filteredItems);
+  if(filteredItems.length === 0){
+      return sectionCards.innerHTML = `
+      <li class="card">
+      <div class="top-card">
+          <h2 class="title">Nenhum item encontrado</h2>
+      </li>
+      `
+  }
+});
 
 const blogSections = [
-    {
-        id:1,
-        title: "Title 1",
-        description: "Description 1 Description 1 Description 1 Description 1 Description 1 Description 1 Description 1 Description 1 Description 1 Description 1 Description 1 Description 1 Description 1 Description 1Description 1 Description 1 Description 1 Description 1 Description 1 Description 1 Description 1Description 1 Description 1 Description 1 Description 1 Description 1 Description 1 Description 1Description 1 Description 1 Description 1 Description 1 Description 1 Description 1 Description 1Description 1 Description 1 Description 1 Description 1 Description 1 Description 1 Description 1Description 1 Description 1 Description 1 Description 1 Description 1 Description 1 Description 1Description 1 Description 1 Description 1 Description 1 Description 1 Description 1 Description 1Description 1 Description 1 Description 1 Description 1 Description 1 Description 1 Description 1",
-        date: "date",
-        like: "y"
-    },
-    {
-        id:2,
-        title: "Title 2",
-        description: "Description 1 Description 1 Description 1 Description 1 Description 1 Description 1 Description 1",
-        date: "date",
-        like: "n"
-    },
-    {   
-        id:3,
-        title: "Title 3",
-        description: "Description 1 Description 1 Description 1 Description 1 Description 1 Description 1 Description 1",
-        date: "date",
-        like: "n"
-    },
-    {   
-        id:4,
-        title: "Title 4",
-        description: "Description 1 Description 1 Description 1 Description 1 Description 1 Description 1 Description 1",
-        date: "date",
-        like: "n"
-    },
-    {
-        id:5,
-        title: "Title 5",
-        description: "Description 1 Description 1 Description 1 Description 1 Description 1 Description 1 Description 1",
-        date: "date",
-        like: "y"
-    },
-    {
-        id:6,
-        title: "Title 6",
-        description: "Description 1 Description 1 Description 1 Description 1 Description 1 Description 1 Description 1",
-        date: "date",
-        like: "y"
-    },
-]
+  {
+    id: 1,
+    title: "Title 1",
+    description:
+      "Description 1 Description 1 Description 1 Description 1 Description 1 Description 1 Description 1 Description 1 Description 1 Description 1 Description 1 Description 1 Description 1 Description 1Description 1 Description 1 Description 1 Description 1 Description 1 Description 1 Description 1Description 1 Description 1 Description 1 Description 1 Description 1 Description 1 Description 1Description 1 Description 1 Description 1 Description 1 Description 1 Description 1 Description 1Description 1 Description 1 Description 1 Description 1 Description 1 Description 1 Description 1Description 1 Description 1 Description 1 Description 1 Description 1 Description 1 Description 1Description 1 Description 1 Description 1 Description 1 Description 1 Description 1 Description 1Description 1 Description 1 Description 1 Description 1 Description 1 Description 1 Description 1",
+    date: "date",
+    like: "y",
+  },
+  {
+    id: 2,
+    title: "Title 2",
+    description:
+      "Description 1 Description 1 Description 1 Description 1 Description 1 Description 1 Description 1",
+    date: "date",
+    like: "n",
+  },
+  {
+    id: 3,
+    title: "Title 3",
+    description:
+      "Description 1 Description 1 Description 1 Description 1 Description 1 Description 1 Description 1",
+    date: "date",
+    like: "n",
+  },
+  {
+    id: 4,
+    title: "Title 4",
+    description:
+      "Description 1 Description 1 Description 1 Description 1 Description 1 Description 1 Description 1",
+    date: "date",
+    like: "n",
+  },
+  {
+    id: 5,
+    title: "Title 5",
+    description:
+      "Description 1 Description 1 Description 1 Description 1 Description 1 Description 1 Description 1",
+    date: "date",
+    like: "y",
+  },
+  {
+    id: 6,
+    title: "Title 6",
+    description:
+      "Description 1 Description 1 Description 1 Description 1 Description 1 Description 1 Description 1",
+    date: "date",
+    like: "y",
+  },
+];
 
 const displayItems = (items) => {
-    const htmlString = items
-        .map((items) => {
-            return `
+  const htmlString = items
+    .map((items) => {
+      return `
             <li class="card">
             <div class="top-card">
                 <div class="date">date</div>
@@ -59,13 +85,15 @@ const displayItems = (items) => {
                 <p class="description">${items.description}</p>
             </li>
             `;
-        })
-        .join('');
-        sectionCards.innerHTML = htmlString;
-}
-
-function loadItems (){
-    displayItems(blogSections);
+    })
+    .join("");
+  sectionCards.innerHTML = htmlString;
+};
+/*
+function loadItems() {
+  displayItems(blogSections);
 }
 
 loadItems();
+*/
+displayItems(blogSections);
