b3b51c0b64318d39719dfb47424a71ad3ed042b9
Leandro Sales <leandrossales.96@gmail.com>
2021-08-03 18:11:18 -0300

    Att display items script

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-const sectionCards = document.querySelector("section.post-section");
+const sectionCards = document.querySelector(".post-section");
 const card = document.querySelector("div.card");
 
 const blogSections = [
@@ -7,49 +7,65 @@ const blogSections = [
         title: "Title 1",
         description: "Description 1 Description 1 Description 1 Description 1 Description 1 Description 1 Description 1 Description 1 Description 1 Description 1 Description 1 Description 1 Description 1 Description 1Description 1 Description 1 Description 1 Description 1 Description 1 Description 1 Description 1Description 1 Description 1 Description 1 Description 1 Description 1 Description 1 Description 1Description 1 Description 1 Description 1 Description 1 Description 1 Description 1 Description 1Description 1 Description 1 Description 1 Description 1 Description 1 Description 1 Description 1Description 1 Description 1 Description 1 Description 1 Description 1 Description 1 Description 1Description 1 Description 1 Description 1 Description 1 Description 1 Description 1 Description 1Description 1 Description 1 Description 1 Description 1 Description 1 Description 1 Description 1",
         date: "date",
-        like: false
+        like: "y"
     },
     {
         id:2,
         title: "Title 2",
         description: "Description 1 Description 1 Description 1 Description 1 Description 1 Description 1 Description 1",
         date: "date",
-        like: false
+        like: "n"
     },
     {   
         id:3,
         title: "Title 3",
         description: "Description 1 Description 1 Description 1 Description 1 Description 1 Description 1 Description 1",
         date: "date",
-        like: false
+        like: "n"
     },
     {   
         id:4,
         title: "Title 4",
         description: "Description 1 Description 1 Description 1 Description 1 Description 1 Description 1 Description 1",
         date: "date",
-        like: false
+        like: "n"
     },
     {
         id:5,
         title: "Title 5",
         description: "Description 1 Description 1 Description 1 Description 1 Description 1 Description 1 Description 1",
         date: "date",
-        like: false
+        like: "y"
     },
     {
         id:6,
         title: "Title 6",
         description: "Description 1 Description 1 Description 1 Description 1 Description 1 Description 1 Description 1",
         date: "date",
-        like: false
+        like: "y"
     },
 ]
 
-blogSections.map(item => {
-    const cardClone = card.cloneNode(true);
-    cardClone.setAttribute("id", item.id);
-    cardClone.querySelector(".title").innerHTML = item.title;
-    cardClone.querySelector(".description").innerHTML = item.description;
-    sectionCards.appendChild(cardClone);
-})
+const displayItems = (items) => {
+    const htmlString = items
+        .map((items) => {
+            return `
+            <li class="card">
+            <div class="top-card">
+                <div class="date">date</div>
+                    <i class="far fa-heart"></i>
+                </div>
+                <h2 class="title">${items.title}</h2>
+                <p class="description">${items.description}</p>
+            </li>
+            `;
+        })
+        .join('');
+        sectionCards.innerHTML = htmlString;
+}
+
+function loadItems (){
+    displayItems(blogSections);
+}
+
+loadItems();
